06a6a621658008dc2a3fca717f405c00c2821ab5
sodprj <morisidhraj001@gmail.com>
2026-08-05 04:46:57 +0530

    INITIAL COMMIT

--- app/config.py
+++
@@ -1,4 +0,0 @@
-import os
-
-class Config:
-    MODEL_NAME = os.getenv("MODEL_NAME","llama3:8b")
--- app/routes/home.py
+++
@@ -1,7 +0,0 @@
-from flask import render_template,Blueprint
-
-home_bp = Blueprint("home",__name__)
-
-@home_bp.route("/",methods=['GET'])
-def home():
-    return render_template("index.html")
